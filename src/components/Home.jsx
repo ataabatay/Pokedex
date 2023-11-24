00912ce4db3c23b6d3d04e3b50054986162292aa
Dan <dan.edmunds7@gmail.com>
2023-11-24 12:16:49 +0000

    pokedex

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
 
     return (
         <div className="home-container">
-            <img id="pokedex" src='../src/assets/images/pokedex.png'/>
+            <img id="pokedex" src='https://i.imgur.com/hpiC22x.png'/>
             <h1 className="title">Illustrated Pokémon Encyclopedia</h1>
             <Button as={Link} to={"/pokemon"} type="button" className="btn btn-outline-warning"><span>Catch 'Em All</span></Button>
             <Button as={Link} to={`/pokemon/${Math.floor((Math.random() * 1017) + 1)}`} type="button"><span>I'm Feeling Lucky</span></Button>
