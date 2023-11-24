1c6d5d328204a2c8e410924d5250b7c7c69ca5b1
Dan <dan.edmunds7@gmail.com>
2023-11-24 13:33:49 +0000

    width issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import { Outlet, useNavigation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 import Nav from './components/Nav'
-import Spinner from 'react-bootstrap/Spinner'
+
 
 function App() {
 
-  const navigation = useNavigation()
+
 
   return (
     <>
       <Nav />
       <main>
-        {navigation.state === 'idle' ?
           <Outlet />
-          :
-          
-             <Spinner className='spinner' animation="border" variant="danger" />
-         
-        }
       </main>
     </>
   )
